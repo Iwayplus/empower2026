863d6cc6fac7e1dc4ca5769b9854bed6321a5a67
codingercat <shambhavithakur9@gmail.com>
2026-08-15 01:50:12 +0530

    updated helpline number

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -248,7 +248,7 @@ const Footer = () => {
                         <Elm>
                             <img src={call} alt="" aria-hidden="true" />
                             <div>
-                                <a href={`tel:${dynamicSection?.content?.contact_phone || "+919717477964"}`}>{dynamicSection?.content?.contact_phone || "+919717477964"}</a>
+                                <a href={`tel:${dynamicSection?.content?.contact_phone || "+918796897338"}`}>{dynamicSection?.content?.contact_phone || "+919717477964"}</a>
                             </div>
                         </Elm>
                         <Elm>
